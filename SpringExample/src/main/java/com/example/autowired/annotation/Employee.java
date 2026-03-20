1a465e9e3e686c2c1118412c29b9fea5a24885c1
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2026-03-20 19:24:42 +0530

    @Autowired Annotation

---
+++ SpringExample/src/main/java/com/example/autowired/annotation/Employee.java
@@ -0,0 +1,34 @@
+package com.example.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component()
+public class Employee {
+    private int employeeId;
+    @Value("Chethmi")
+    private String firstName;
+    @Value("Nayanathara")
+    private String lastName;
+    @Value("#{4*9*10000}")
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
