90431e6728d87903b609dadfd9753340d426fad9
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2026-03-21 07:56:57 +0530

    Create sample REST API

---
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -0,0 +1,21 @@
+package com.embarkx.FirstSpring;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name){
+        return new HelloResponse("Hello " + name);
+    }
+
+    @GetMapping("/hello")
+    public HelloResponse helloParam(){
+        return new HelloResponse("Hello World!");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello " + name + "!");
+    }
+}
---
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloResponse.java
@@ -0,0 +1,17 @@
+package com.embarkx.FirstSpring;
+
+public class HelloResponse {
+    private String message;
+
+    public HelloResponse(String s) {
+        this.message = s;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
